1fda8226abe2da974022a2f1f8aef5dd8c185db1
consonant <consonant1999@gmail.com>
2017-06-27 20:36:12 +0800

    Completed large hero block and intro content block

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// var gulp = require('gulp');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+
+
+// gulp.task('default',function(){
+//   console.log("First gulp task");
+// });
+//
+// gulp.task('html',function(){
+//   console.log("html bla bla bla...");
+// });
