d6fd475a8c588edab94a4a47e97f86a496172a25
consonant <consonant1999@gmail.com>
2017-07-15 19:37:02 +0800

    Compeleted sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -20,7 +20,9 @@
 // $('h1').remove();
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"),"60%");
+var stickyHeader = new StickyHeader();
